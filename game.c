1004fc7d8159b699d7bbe739963791a6a38f5325
Pang <psu43@uclive.ac.nz>
2016-10-08 01:34:19 +1300

    ball bounces

--- game.c
+++ game.c
@@ -18,11 +18,11 @@
 #define DISPLAY_TASK_RATE 250
 #define BUTTON_TASK_RATE 100
 
-
 int prev_column = 0;
+bool flyout = false;
 uint8_t current_column = 0;
+uint8_t flybit;
 bool ball_shot = false;
-bool start_shot = false;
 /** Define PIO pins driving LED matrix rows.  */
 static const pio_t rows[] =
 {
@@ -68,7 +68,8 @@ static unsigned char lookup[16] = {
 0x0, 0x8, 0x4, 0xc, 0x2, 0xa, 0x6, 0xe,
 0x1, 0x9, 0x5, 0xd, 0x3, 0xb, 0x7, 0xf, };
 
-
+int bounce_to;
+int bounce_from;
 int prev_shot_row = 3;
 int new_shot_row = 2;
 bool received = false;
@@ -76,9 +77,8 @@ bool received = false;
 int receive_go_to = 1;
 int receive_prev = 0;
 bool bounce = false;
-int start_with_ball = 0;
 int communicated = 0;
-
+bool opp_start = false;
 
 /*
 uint8_t reverse(uint8_t x)
@@ -95,61 +95,22 @@ uint8_t reverse(uint8_t n) {
 	return (lookup[n&0b1111] << 3) | lookup[n>>3];
 }
 
-static void board(__unused__ void *data) {
 
-    display_column (bitmap[current_column], current_column);
-
-    current_column++;
-
-    if (current_column > (LEDMAT_COLS_NUM - 1))
-    {
-        current_column = 0;
-    }
-    
-    // send ball to opponent once the ball is at the edge of the screen
-    if (ball_shot && bitmap[0] !=  0) {
-		ir_uart_putc(bitmap[0]); // send ball
-		bitmap[0] = 0x00; // because ball has left your screen
-	}
-	
-	// ready to receive 
-	if(ir_uart_read_ready_p()){
-		if(communicated == 1){
-			
-			uint8_t rec_char = ir_uart_getc();
-			// if receive signal from opp, you get the ball
-			if(rec_char == 'S'){
-				communicated = 2; // made contact with opp
-				// give ball
-				bitmap[3] = 0b0001000;
-				// tell opp you got ball
-				ir_uart_putc('B');
-			}
-			
-			if(rec_char == 'B'){
-				// opponent has the ball
-				communicated = 2;
-			}
-			
-		}
-		else{
-			// should receive a ball
-			//bitmap[0] = ir_uart_getc();
-			bitmap[0] = reverse(ir_uart_getc());
-			//received = true;
-		}
-		/*
-		else{
-			bitmap[0] = rec_char;
-		}
-		* */
-		// received ball at col 0 so keep receiving ball
-		//received = true;
-		
-	}
-
-
+/*
+// encrypt ball bitmap into condensed form
+// eg. takes 1000000 and turns into 111
+uint8_t encrypt_ball(uint8_t bits)
+{
+	return log(bits) / log(2) + 1;
 }
+
+// decrypt ball bitmap into true form
+// eg takes 111 turns into 1000000
+uint8_t decrypt_ball(uint8_t bits)
+{
+	return pow(2,bits);
+}
+* */
 
 
 
@@ -172,6 +133,71 @@ void display_column (uint8_t row_pattern, uint8_t current_column)
     
 
 }
+
+static void board(__unused__ void *data) {
+
+    display_column (bitmap[current_column], current_column);
+
+    current_column++;
+
+    if (current_column > (LEDMAT_COLS_NUM - 1))
+    {
+        current_column = 0;
+    }
+    
+    // send ball to opponent once the ball is at the edge of the screen
+    if ((ball_shot && bitmap[0] !=  0) || (bounce && bitmap[0] != 0) ) {
+		ir_uart_putc(bitmap[0]); // send ball
+		//uint8_t e = encrypt_ball(bitmap[0]);
+		//ir_uart_putc(e);
+		bitmap[0] = 0x00; // because ball has left your screen
+	}
+	
+	// ready to receive 
+	if(ir_uart_read_ready_p()){
+		if(communicated == 1){
+			
+			uint8_t rec_char = ir_uart_getc();
+			// if receive signal from opp, you get the ball
+			if(rec_char == 'S'){
+				communicated = 2; // made contact with opp
+				// give ball
+				bitmap[3] = 0b0001000;
+				// tell opp you got ball
+				ir_uart_putc('B');
+			}
+			
+			if(rec_char == 'B'){
+				// opponent has the ball
+				opp_start = true;
+				communicated = 2;
+			}
+			
+		}
+		else{
+			// should receive a ball
+			//bitmap[0] = log(ir_uart_getc())/log(2);
+			bitmap[0] = reverse(ir_uart_getc());
+			// this shall convert the read bitmap
+			//bitmap[0] = log(reverse(ir_uart_getc()))/log(2) + 1;
+			received = true;
+		}
+		/*
+		else{
+			bitmap[0] = rec_char;
+		}
+		* */
+		// received ball at col 0 so keep receiving ball
+		//received = true;
+		
+	}
+
+
+}
+
+
+
+
 
 
 
@@ -222,14 +248,13 @@ static void button_task (__unused__ void *data)
 		  }
 		}
 
-		// push button and ball hasn't been shot yet
-		if (ball_shot == false && navswitch_push_event_p (NAVSWITCH_PUSH))
+		// push button and ball hasn't been shot yet and opp hasnt got ball at start
+		if (opp_start == false && ball_shot == false && navswitch_push_event_p (NAVSWITCH_PUSH))
 		{
-		  start_shot = true;
 		  ball_shot = true; // shoot ball
 		}
 
-		// if ball shot, show ball across screen if the ball is not at col 0
+		// if ball shot, show ball across screen til col 0
 		if(ball_shot && new_shot_row >= 0){
 			bitmap[new_shot_row] = bitmap[prev_shot_row];
 			bitmap[prev_shot_row] = 0x00;
@@ -259,6 +284,44 @@ static void button_task (__unused__ void *data)
 			*/
 		
 		}
+		
+		if(flyout){
+			bitmap[4] = flybit;
+			flyout = false;
+		}
+		
+		// received ball and check if ball clashed
+		if(received && receive_go_to == 4){
+			// check for clashing
+			if((bitmap[4]|bitmap[3]) != bitmap[4]){
+				// not on bat, make ball fly out
+				flybit = bitmap[4];
+				bitmap[4] = bitmap[4]|bitmap[3];
+				bitmap[3] = 0x00;
+				flyout = true;
+			}
+			else{
+				// get ball ready to bounce back
+				bounce_from = 3;
+				bounce_to = 2;
+				bounce = true;
+			}
+			
+			//reset to receive later
+			receive_go_to = 1;
+			receive_prev = 0;
+			received = false;
+		
+		}
+		
+		if(bounce && bounce_to >= 0){
+			bitmap[bounce_to] = bitmap[bounce_from];
+			bitmap[bounce_from] = 0x00;
+			bounce_from--;
+			bounce_to--;
+		}
+		
+	
 		
 	}
 /*	
